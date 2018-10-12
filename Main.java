7d088758b92c45fc3b21d431dd2dd36aba4faade
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2018-10-12 16:47:13 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.awt.*;
+
+import static com.company.Colors.ANSI_PURPLE;
+import static com.company.PoliceClass.runWatcher;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        runWatcher();
+
+
+
+//        System.out.println(ANSI_PURPLE + "ERROR MESSAGE IN RED");
+    }
+}
