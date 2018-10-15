e73f34e49b3d2cda2b4afa837b2010d2896bf808
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2018-10-15 18:36:25 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,146 @@
 package com.company;
 
-import java.awt.*;
+import javafx.concurrent.Task;
 
-import static com.company.Colors.ANSI_PURPLE;
+import static com.company.Colors.ANSI_BLACK_BACKGROUND;
+import static com.company.Colors.ANSI_RED;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_CREATE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_DELETE;
+import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 
-public class Main extends Thread{
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.sql.Timestamp;
+import java.util.*;
 
+class PoliceClass extends Task implements Runnable {
+    private final Scanner sc = new Scanner(System.in);
+    int pathsNumber=0;
+    boolean listening = false;
+    long counter = 0;
+    WatchService watcher;
+    final List<Path> listDir = new ArrayList<>();
+    //    static Path dir,dir2,dir3;
+    Calendar calendar = null;
+    Date date = null;
+    Timestamp timestamp = null;
+    String currentPath=null;
+    final List<String> pathList = new ArrayList<>();
+
+    //    static String directory = "C:\\Users\\taozen\\Desktop";
+//    static String directory3 = "C:\\Users\\taozen\\Downloads";
+//    static String directory2 = "C:\\Users\\taozen\\Desktop\\ANDROID\\testingLinuxAndroid\\app\\src\\main\\java\\com\\example\\tao\\myapplication";
     private static void print(String string) {
         System.out.println(string);
     }
-    public static void main(String[] args) {
-//        print(Thread.getAllStackTraces().toString());
+    @SuppressWarnings("ThrowablePrintedToSystemOut")
+    MyWatcher myWatcher = () -> {
+        print("Welcome to tao\'s listener v.0.1 !" + "\n\n");
+        print("How many paths do you want to listen ?");
+        pathsNumber=Integer.parseInt(sc.nextLine());
+        print("paths number: " + pathsNumber);
+        print("Enter paths: ");
 
-        PoliceClass policeClass = new PoliceClass();
-        policeClass.run();
+        for (int i=0;i<pathsNumber;i++){
+            currentPath=sc.nextLine();
+            pathList.add(String.valueOf(currentPath));
+            print("current path: " + currentPath + "\n" + "pathlist size: " + pathList.size() + " path name: " + pathList.get(i));
+        }
+        try {
+            watcher = FileSystems.getDefault().newWatchService();
+
+            for (String p : pathList){
+                listDir.add(Paths.get(p));
+                print("pathList = " + p);
+            }
+            for (Path pat: listDir){
+                pat.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+                print("Watch Service registered for dir: " + pat.getFileName());
+            }
+
+//solved
+//            dir = Paths.get(directory);
+//            dir2 = Paths.get(directory2);
+//            dir3 = Paths.get(directory3);
+//
+//            dir.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+//            dir2.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+//            dir3.register(watcher, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+//
+//            print("Watch Service registered for dir: " + dir.getFileName());
+//            print("Watch Service registered for dir: " + dir2.getFileName());
+//            print("Watch Service registered for dir: " + dir3.getFileName());
+
+            Thread.sleep(50);
+            //creating listener
+//            while (true)  same as for (;;)
+            for (;;) {
+                WatchKey key;
+                try {
+                    key = watcher.take();
+                    Thread.sleep(50);
+                } catch (InterruptedException ex) {
+                    return;
+                }
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    counter++;
+                    String c = "Counter: " + counter;
+                    WatchEvent.Kind<?> kind = event.kind();
+                    @SuppressWarnings("unchecked")
+                    WatchEvent<Path> ev = (WatchEvent<Path>) event;
+                    Path fileName = ev.context();
+                    calendar = Calendar.getInstance();
+                    date = calendar.getTime();
+                    timestamp = new Timestamp(date.getTime());
+
+                    print(ANSI_BLACK_BACKGROUND + ANSI_RED + kind.name() + ": " + fileName + " \ntimestamp: " + timestamp + "\n" + c);
+//                    if (kind == ENTRY_MODIFY &&
+//                            fileName.toString().equalsIgnoreCase("message_guid_callback.txt")) {
+////                        print("My source file has changed!!! :" + fileName + "\n");
+//                    }
+                }
+                boolean valid = key.reset();
+                if (!valid) {
+                    break;
+                }
+            }
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+    };
+
+    @Override
+    protected Object call() {
+        System.out.println("...processing");
+        return null;
+    }
+
+    @Override
+    public void run() {
 
         Thread.State currState = Thread.currentThread().getState();
         if (currState != Thread.State.NEW && currState != Thread.State.TERMINATED){
-            print("ThreaD Main: " + currState);
+            print("ThreaD PoliceClass: " + currState);
+        }
+        listening = true;
+        while(listening) {
+            try {
+                myWatcher.runWatcher();
+                print(Thread.getAllStackTraces().toString());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
-//        System.out.println(ANSI_PURPLE + "ERROR MESSAGE IN RED");
+        try {
+            watcher.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
